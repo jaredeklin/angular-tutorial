88c88d8ae6b878daed26ab54b4c435365115c7d8
Jared Eklin <jaredeklin@gmail.com>
2018-08-24 15:51:48 -0600

    Complete hero editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
-@NgModule({
+@NgModule({ 
   declarations: [
     AppComponent,
     HeroesComponent
