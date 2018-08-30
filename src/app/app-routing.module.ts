798c3c251357fefcab45f3fc0cbd09bad4f74d94
Jared Eklin <jaredeklin@gmail.com>
2018-08-30 11:31:04 -0600

    Add router nav link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent } from "./heroes/heroes.component";
 
-const routes: Routes [
-  { path: 'heroes', component: HeroesComponent}
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
 ]
 
 @NgModule({
