068d3170ce237f5ebab3a7bdef0b1304c993dc25
Jared Eklin <jaredeklin@gmail.com>
2018-08-30 11:07:18 -0600

    Add RouterModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router'
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  exports: [
+    RouterModule
+  ]
 })
+
 export class AppRoutingModule { }
