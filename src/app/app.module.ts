ad91ca0b5acc011883a9dba5c7729cf7653d6734
Jared Eklin <jaredeklin@gmail.com>
2018-09-05 17:40:18 -0600

    Add HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({ 
   declarations: [
