d09a0246a3ee402b2e888c09f2fabbb8ca2c1b19
Jared Eklin <jaredeklin@gmail.com>
2018-09-06 13:26:45 -0600

    Add handleError

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -32,6 +32,15 @@ export class HeroService {
       )
   }
 
+  private handleError<T>(operation = 'operation', result?:T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      this.log(`${operation} failed: ${error.message}`)
+      
+      return of(result as T)
+    }
+  }
+
   
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`)
